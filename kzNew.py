e653b50220728e4910c4d3b7ca3b5184d471bd7c
FedulovaKarina <37471501+FedulovaKarina@users.noreply.github.com>
2018-03-28 22:47:56 +0300

    Add files via upload

--- kzNew.py
+++ kzNew.py
@@ -1,56 +1,70 @@
+#Spinkoins
+#область работы карины
 
+#подключение функции рандома
+import random
 
+#задаем рандомное число для отгадывания
+random_num=random.randint(1000,9999)
+#переводим рандомное число в строку для проверки
+stroka_randoma=str(random_num)
+#начало рекурсивной ловушки для рандомного числа
+if stroka_randoma[0]!=stroka_randoma[1]!=stroka_randoma[2]!=stroka_randoma[3]:
+        1+1
+else  :
+        while stroka_randoma[0]==stroka_randoma[1] or stroka_randoma[0]==stroka_randoma[2] or stroka_randoma[0]==stroka_randoma[3] or 
+stroka_randoma[1]==stroka_randoma[2] or stroka_randoma[1]==stroka_randoma[3] or stroka_randoma[2]==stroka_randoma[3] :
+                stroka_randoma=str(random.randint(1000,9999))
+
+#конец ловушки
+
+print(q)
 
 #начало функции по вводу пользовательского числа
 def num():
-
+#!!!!проблемный блок кода (невыходящая рекурсия)
 	#вводим пользовательское число
-	num=int(input())
-
+	n=str(input())
 	#ограничение для числа and рекурсивная ловушка
+	l=len(n)
+	if l==3 and n[0]!=n[1]!=n[2]!=n[3] and n[1]!=n[0]!=n[2]!=n[3] and n[2]!=n[0]!=n[1]!=n[3] and n[3]!=n[0]!=n[1]!=n[2]:
+        	print("OK")
+	elif l<4 or l>4 or [0]==n[1] or n[0]==n[2] or n[0]==n[3] or n[1]==n[2] or n[1]==n[3] or n[2]==n[3]:
+        	while l<4 or l>4 or [0]==n[1] or n[0]==n[2] or n[0]==n[3] or n[1]==n[2] or n[1]==n[3] or n[2]==n[3]:
+                    n=str(input())
+                    print("False")
 
-	if 999<num<10000:
-        	print("")
-	elif num<999 or num>10000:
-        	while num<999 or num>10000:
-                	num=int(input())
+		
 
-	#переводим число в строку для проверки
-	n=str(num)
+	#переводим число в строку для проверк
 	#начало второй рекурсивной ловушки
-	if n[0]!=n[1]!=n[2]!=n[3] and n[1]!=n[0]!=n[2]!=n[3] and n[2]!=n[0]!=n[1]!=n[3] and n[3]!=n[0]!=n[1]!=n[2]:
-       		 print("Cпасибо ваше число правильно")
-	elif n[0]==n[1] or n[0]==n[2] or n[0]==n[3] or n[1]==n[2] or n[1]==n[3] or n[2]==n[3]  :
-       		 while n[0]==n[1] or n[0]==n[2] or n[0]==n[3] or n[1]==n[2] or n[1]==n[3] or n[2]==n[3] :
-                	n=str(input())
+	#if n[0]!=n[1]!=n[2]!=n[3] and n[1]!=n[0]!=n[2]!=n[3] and n[2]!=n[0]!=n[1]!=n[3] and n[3]!=n[0]!=n[1]!=n[2]:
+       	#	 1+1
+	#elif n[0]==n[1] or n[0]==n[2] or n[0]==n[3] or n[1]==n[2] or n[1]==n[3] or n[2]==n[3]  :
+       	#	 while n[0]==n[1] or n[0]==n[2] or n[0]==n[3] or n[1]==n[2] or n[1]==n[3] or n[2]==n[3] :
+        #        	n=str(input())
 
 	return n
 #конец функции 
 
 
 #начало функции топоры и тузы
-dev Ace_Axe(n,q):
+def Ace_Axe(n,q):
 
-	Начало реализации правил игры
-
-	#print("Число игрока:"+n)
-	#print("Число что задала система:"+q)
-	#i=int(0)
-	#c=int(0)
-
+	#Начало реализации правил игры
 	#система работы топоров
+	i=0
+	c=0
 
 	for i in range(4):
        		 if q[i]==n[i]:
                 	c=c+1
        		 else :
                 	i=i+1
-
-
-	#c=int(c)
+	
 	print ('Вы имеете'+' '+str(c)+' '+'Топора')
 	#cистема работы топоров отлажена
-
+	
 	#система работы тузов	
 	r=0
 	g=0
@@ -66,10 +80,22 @@ dev Ace_Axe(n,q):
 	d=d-c
 	print ('Вы имеете'+' '+str(d)+' '+'Туза')
 	#cистема работы тузов отлажена
-
-	return d
 	return c
+
 #конец функии 
- 
-num()
-Ace_Axe()
+
+#функция самой игры 
+
+def open(num,stroka_randoma):
+	z=num()
+	c=Ace_Axe(z,stroka_randoma)
+	return c
+#конец функции
+p=open(num,stroka_randoma)
+#условие цикличности
+if p==4:
+	print("OK")
+elif p!=4 or p==0 :
+	while p!=4 :
+		p  #=open(num,q)
+#Конец
